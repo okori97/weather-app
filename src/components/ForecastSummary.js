900d6bb566f9e124f4f7b9ae82ca97a6f1a5e600
Okori McCalla <okorishakir@gmail.com>
2023-07-21 08:42:14 +0100

    accessed nested max temperature property and passed test

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -1,11 +1,13 @@
 import React from "react";
 import forecast from "../data/forecast.json";
 
-export default function ForecastSummary(props) {
-  const date = "11";
+const { forecasts } = forecast;
+
+export default function ForecastSummary() {
+  const { temperature } = forecasts[0];
   return (
     <div>
-      <h1>{date}</h1>
+      <h1>{temperature.max}</h1>
     </div>
   );
 }
