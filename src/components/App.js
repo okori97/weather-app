63ea9858e50201cad93e9f40bd51cf79a15d76d8
Okori McCalla <okorishakir@gmail.com>
2023-07-23 14:20:03 +0100

    Added forecastSummary component to App and updated the component to render more properties

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import "../styles/App.css";
 import LocationDetails from "./LocationDetails";
+import ForecastSummary from "./ForecastSummary";
 
-const App = ({ location }) => {
+const App = ({ location }, { forecasts }) => {
   const { city, country } = location;
   return (
     <div>
       <div>
         <LocationDetails city={city} country={country} />
+        <ForecastSummary forecast={forecasts} />
       </div>
     </div>
   );
--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -5,9 +5,38 @@ const { forecasts } = forecast;
 
 export default function ForecastSummary() {
   const { temperature } = forecasts[0];
+  const { icon } = forecasts[0];
+  const { description } = forecasts[0];
+
+  // gets date
+  const dateObj = new Date(forecasts[0].date * 1000);
+  const dayNum = dateObj.getDate();
+  const month = dateObj.toLocaleDateString("en-UK", { month: "short" });
+  const day = dateObj.toLocaleDateString("en-UK", { weekday: "short" });
+  const suffix = (number) => {
+    if (number > 3 && number < 30) return "th";
+
+    switch (number) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        break;
+    }
+  };
+
+  const date = `${day} ${dayNum}${suffix(dayNum)} ${month}`;
+
+  console.log(date);
   return (
-    <div>
-      <h1>{temperature.max}</h1>
+    <div className="block__summary">
+      <div className="block__date">{date}</div>
+      <div className="block__icon">{icon}</div>
+      <div className="block__temparature">{`${temperature.max}°C`}</div>
+      <div className="block__description">{description}</div>
     </div>
   );
 }
