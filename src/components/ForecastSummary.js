336603554a013f44e7f7fd387bbcb5f6d28f2935
Okori McCalla <okorishakir@gmail.com>
2023-07-26 17:44:01 +0100

    Updated tests for ForecastSummary component using asFragment and snapshots

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -8,8 +8,10 @@ export default function ForecastSummary(props) {
   return (
     <div className="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
-      <div className="forecast-summary__icon">{icon}</div>
-      <div className="forecast-summary__temparature">{`${temperature}°C`}</div>
+      <div className="forecast-summary__icon" data-testid="forecast-icon">
+        {icon}
+      </div>
+      <div className="forecast-summary__temperature">{`${temperature.max}°C`}</div>
       <div className="forecast-summary__description">{description}</div>
     </div>
   );
--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -3,9 +3,47 @@ import { render } from "@testing-library/react";
 import ForecastSummary from "../../components/ForecastSummary";
 
 describe("ForecastSummary", () => {
-  it("renders the correct max temperature", () => {
-    const { getByText } = render(<ForecastSummary max="11" />);
+  const validProps = {
+    date: 1111111,
+    description: "stubDescription",
+    icon: "stubIcon",
+    temperature: {
+      max: 11,
+      min: 4,
+    },
+  };
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
 
-    expect(getByText("11")).toBeInstanceOf(HTMLHeadingElement);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders the correct value for props", () => {
+    const { getByText, getByTestId } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+
+    expect(getByText("1111111")).toHaveAttribute(
+      "class",
+      "forecast-summary__date"
+    );
+    expect(getByText("stubDescription")).toHaveClass(
+      "forecast-summary__description"
+    );
+    expect(getByText("stubIcon")).toHaveClass("forecast-summary__icon");
+    expect(getByText("11°C")).toHaveClass("forecast-summary__temperature");
+    expect(getByTestId("forecast-icon")).toHaveClass("forecast-summary__icon");
   });
 });
