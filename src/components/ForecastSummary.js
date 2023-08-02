6ed7360646c48233b44e706a62f4467f34bbf7f4
Okori McCalla <okorishakir@gmail.com>
2023-08-02 22:11:03 +0100

    converted unix into date within forecastSummary component

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -3,9 +3,33 @@ import React from "react";
 export default function ForecastSummary(props) {
   const { date, description, temperature, icon } = props;
 
+  // Converts unix into day and month
+  let newDate = new Date(date);
+  const monthNum = newDate.getDate();
+  const monthString = newDate.toLocaleString("en-US", { month: "short" });
+  const day = newDate.toLocaleString("en-US", { weekday: "short" });
+
+  // grabs date and adds the proper suffix
+  const dateFormat = (month) => {
+    if (month > 3 && month < 21) return `${month}th`;
+    switch (month) {
+      case 1:
+        return `${month}st`;
+      case 2:
+        return `${month}nd`;
+      case 3:
+        return `${month}rd`;
+
+      default:
+        return `${month}th`;
+    }
+  };
+
+  let convertedDate = `${day} ${dateFormat(monthNum)} ${monthString}`;
+
   return (
     <div className="forecast-summary">
-      <div className="forecast-summary__date">{date}</div>
+      <div className="forecast-summary__date">{convertedDate}</div>
       <div className="forecast-summary__icon" data-testid="forecast-icon">
         {icon}
       </div>
