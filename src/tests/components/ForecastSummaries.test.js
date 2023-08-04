851845a82b1fae3b6531ad71f0a36edb4ec45b15
Okori McCalla <okorishakir@gmail.com>
2023-08-04 20:37:54 +0100

    added snapshot and test for list length for ForecastSummaries component

---
+++ src/tests/components/ForecastSummaries.test.js
@@ -0,0 +1,40 @@
+import ForecastSummaries from "../../components/ForecastSummaries";
+import ForecastSummary from "../../components/ForecastSummary";
+import React from "react";
+import { render } from "@testing-library/react";
+
+describe("forecastSummaries", () => {
+  const validProp = [
+    {
+      date: 111,
+      description: "stubDescription",
+      temperature: {
+        max: 10,
+        min: 10,
+      },
+      icon: "stubIcon",
+    },
+    {
+      date: 222,
+      description: "stubDescription",
+      temperature: {
+        max: 10,
+        min: 10,
+      },
+      icon: "stubIcon",
+    },
+  ];
+
+  it("it renders correctly", () => {
+    const { asFragment } = render(<ForecastSummaries forecasts={validProp} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders the correct number of list items", () => {
+    const { getAllByTestId } = render(
+      <ForecastSummaries forecasts={validProp} />
+    );
+
+    expect(getAllByTestId("forecast-summary")).toHaveLength(2);
+  });
+});
