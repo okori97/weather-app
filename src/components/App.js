3c0da3a4b396d4d27e5a9433424b9fb3f5d0ca15
Okori McCalla <okorishakir@gmail.com>
2023-08-10 19:52:14 +0100

    Added state into App component and created an event handler variable

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,26 @@ import "../styles/index.css";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
+import React, { useState } from "react";
 
 const App = ({ location, forecasts }) => {
+  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+  const selectedForecast = forecasts.find(
+    (forecast) => forecast.date === selectedDate
+  );
+
+  const handleForecastSelect = (date) => {
+    setSelectedDate(date);
+  };
   return (
     <div className="weather-app">
       <div>
         <LocationDetails city={location.city} country={location.country} />
-        <ForecastSummaries forecasts={forecasts} />
-        <ForecastDetails forecasts={forecasts} />
+        <ForecastSummaries
+          forecasts={forecasts}
+          onForecastSelect={handleForecastSelect}
+        />
+        <ForecastDetails forecast={selectedForecast} />
       </div>
     </div>
   );
