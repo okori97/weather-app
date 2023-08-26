90d5e7d0eba21a10d09d96114e5c9c9db362cd29
Okori McCalla <okorishakir@gmail.com>
2023-08-26 14:05:09 +0100

    update getForecasts to expect a place parameter, and use place if it exists, allowing search reqs

--- src/requests/getForecasts.js
+++ src/requests/getForecasts.js
@@ -3,13 +3,22 @@ import axios from "axios";
 export default function getForecasts(
   setLocation,
   setForecasts,
-  setSelectedDate
+  setSelectedDate,
+  place
 ) {
-  const endpoint =
-    "https://cmd-shift-weather-app.onrender.com/forecast?city=Cambridge";
-  axios.get(endpoint).then((response) => {
-    setLocation(response.data.location);
-    setForecasts(response.data.forecasts);
-    setSelectedDate(response.data.forecasts[0].date);
-  });
+  let endpoint;
+
+  place
+    ? (endpoint = `https://cmd-shift-weather-app.onrender.com/forecast?city=${place}`)
+    : (endpoint =
+        "https://cmd-shift-weather-app.onrender.com/forecast?city=Cambridge");
+
+  axios
+    .get(endpoint)
+    .then((response) => {
+      setLocation(response.data.location);
+      setForecasts(response.data.forecasts);
+      setSelectedDate(response.data.forecasts[0].date);
+    })
+    .catch(Error);
 }
