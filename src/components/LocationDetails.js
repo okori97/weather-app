bd13c7f882524ff256abd09a98d03a10be530a48
Okori McCalla <okorishakir@gmail.com>
2023-08-27 11:01:26 +0100

    Show only error message and search bar when there is an error so user is not confused.

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -4,10 +4,14 @@ export default function LocationDetails(props) {
   const { city, country, isError } = props;
   console.log(isError);
 
+  //Returns nothing if there is an error
+  if (isError) {
+    return <>{isError}</>;
+  }
+
   return (
     <>
       <h1>{`${city}, ${country}`}</h1>
-      {isError && <>{isError}</>}
     </>
   );
 }
