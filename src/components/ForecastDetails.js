fd04ddbbee85534090c43e77c57c8c8522522579
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-08 21:14:36 +0000

    update scripts which depend on getDate util to import it by name rather than as default export

--- src/components/ForecastDetails.js
+++ src/components/ForecastDetails.js
@@ -1,39 +1,39 @@
 import React from "react";
-import GetDate from "../helpers/getDate";
+import { GetDate } from "../helpers/dateUtils";
 import addSuffix from "../helpers/addSuffix";
 import "../styles/ForecastDetails.css";
 
 export default function ForecastDetails({ forecast, isError }) {
-  const { date, temperature, humidity, wind } = forecast;
+	const { date, temperature, humidity, wind } = forecast;
 
-  //Returns nothing if there is an error
-  if (isError) {
-    return <div></div>;
-  }
+	//Returns nothing if there is an error
+	if (isError) {
+		return <div></div>;
+	}
 
-  // Converts unix into date and adds suffix to month
-  const { day, monthNum, monthString } = GetDate(date);
-  let convertedDate = `${day} ${addSuffix(monthNum)} ${monthString}`;
+	// Converts unix into date and adds suffix to month
+	const { day, monthNum, monthString } = GetDate(date);
+	let convertedDate = `${day} ${addSuffix(monthNum)} ${monthString}`;
 
-  return (
-    <div className="forecast-details">
-      <h2>{convertedDate}</h2>
-      <div className="forecast-details--item">
-        <span className="bold">Max Temperature: </span>
-        {`${temperature.max}°c`}
-      </div>
-      <div className="forecast-details--item">
-        <span className="bold">Min Temperature: </span>
-        {`${temperature.min}°c`}
-      </div>
-      <div className="forecast-details--item">
-        <span className="bold">Humidity: </span>
-        {`${humidity}%`}
-      </div>
-      <div className="forecast-details--item">
-        <span className="bold">Wind: </span>
-        {wind.speed}
-      </div>
-    </div>
-  );
+	return (
+		<div className="forecast-details">
+			<h2>{convertedDate}</h2>
+			<div className="forecast-details--item">
+				<span className="bold">Max Temperature: </span>
+				{`${temperature.max}°c`}
+			</div>
+			<div className="forecast-details--item">
+				<span className="bold">Min Temperature: </span>
+				{`${temperature.min}°c`}
+			</div>
+			<div className="forecast-details--item">
+				<span className="bold">Humidity: </span>
+				{`${humidity}%`}
+			</div>
+			<div className="forecast-details--item">
+				<span className="bold">Wind: </span>
+				{wind.speed}
+			</div>
+		</div>
+	);
 }
--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -1,37 +1,37 @@
 import React from "react";
 import iconData from "../data/iconData.json";
 import "../styles/ForecastSummary.css";
-import GetDate from "../helpers/getDate";
+import { GetDate } from "../helpers/dateUtils";
 import addSuffix from "../helpers/addSuffix";
 
 export default function ForecastSummary(props) {
-  const { date, description, temperature, icon, onSelect } = props;
-  const iconCode = icon.toString().slice(0, 1) + "00";
+	const { date, description, temperature, icon, onSelect } = props;
+	const iconCode = icon.toString().slice(0, 1) + "00";
 
-  // Converts unix into date and adds suffix to month
-  const { day, monthNum, monthString } = GetDate(date);
-  let convertedDate = `${day} ${addSuffix(monthNum)} ${monthString}`;
+	// Converts unix into date and adds suffix to month
+	const { day, monthNum, monthString } = GetDate(date);
+	let convertedDate = `${day} ${addSuffix(monthNum)} ${monthString}`;
 
-  return (
-    <div className="forecast-summary" data-testid="forecast-summary">
-      <div className="forecast-summary__date">{convertedDate}</div>
-      <img
-        className="forecast-summary__icon"
-        data-testid="forecast-icon"
-        src={iconData[iconCode]}
-        alt="weather icon"
-      ></img>
-      <div className="forecast-summary__temperature">{`${temperature.max}°c`}</div>
-      <div className="forecast-summary__description">{description}</div>
-      <div className="forecast-summary__link-wrap">
-        <button
-          type="button"
-          className="forecast-summary__link"
-          onClick={() => onSelect(date)}
-        >
-          More details
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="forecast-summary" data-testid="forecast-summary">
+			<div className="forecast-summary__date">{convertedDate}</div>
+			<img
+				className="forecast-summary__icon"
+				data-testid="forecast-icon"
+				src={iconData[iconCode]}
+				alt="weather icon"
+			></img>
+			<div className="forecast-summary__temperature">{`${temperature.max}°c`}</div>
+			<div className="forecast-summary__description">{description}</div>
+			<div className="forecast-summary__link-wrap">
+				<button
+					type="button"
+					className="forecast-summary__link"
+					onClick={() => onSelect(date)}
+				>
+					More details
+				</button>
+			</div>
+		</div>
+	);
 }
