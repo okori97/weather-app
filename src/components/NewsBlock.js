0ea16aa214c51c04a77b1645c55621bd78d89dfb
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-08 20:55:49 +0000

    import getNews into NewsBlock and use effect to call it on mount and set results to state

--- src/components/NewsBlock.js
+++ src/components/NewsBlock.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/NewsBlock.css";
+import getNews from "../requests/getNews";
 
 export default function NewsBlock() {
+	const [news, setNews] = useState({
+		title: "",
+		url: "",
+		name: "",
+		publishedAt: "",
+	});
+	useEffect(() => {
+		async function fetchNews() {
+			const response = await getNews();
+			setNews(response);
+		}
+		console.log(news.publishedAt);
+		fetchNews();
+	}, []);
+
 	return (
 		<div className="news space-between">
 			<div className="news__details">
-				<h1 className="news__details-title">
-					UK weather: Snow warning for some as mild conditions to end
-				</h1>
+				<h1 className="news__details-title"> {news.title}</h1>
 				<p className="news__details-timestamp">5 mins ago</p>
 			</div>
 
 			<div className="news__row space-between">
-				<a className="news__button" href="#" target="_blank">
+				<a className="news__button" href={news.url} target="_blank">
 					Read More
 				</a>
-				<div className="news__chip">BBC News</div>
+				<div className="news__chip">{news.name}</div>
 			</div>
 		</div>
 	);
