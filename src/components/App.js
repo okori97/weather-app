f7a90d16f5a6de00136e46ea658221c1da0bdc20
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-08 17:51:27 +0000

    wrap the components, inside app, ahead of creating a modal for main content

--- src/components/App.js
+++ src/components/App.js
@@ -8,58 +8,74 @@ import React, { useState, useEffect } from "react";
 import getForecasts from "../requests/getForecasts";
 
 const App = () => {
-  const [location, setLocation] = useState({});
-  const [forecasts, setForecasts] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(0);
-  const [searchText, setSearchText] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  console.log(searchText);
+	const [location, setLocation] = useState({});
+	const [forecasts, setForecasts] = useState([]);
+	const [selectedDate, setSelectedDate] = useState(0);
+	const [searchText, setSearchText] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	console.log(searchText);
 
-  useEffect(() => {
-    getForecasts(setErrorMessage, setLocation, setForecasts, setSelectedDate);
-  }, []);
+	useEffect(() => {
+		getForecasts(
+			setErrorMessage,
+			setLocation,
+			setForecasts,
+			setSelectedDate
+		);
+	}, []);
 
-  const selectedForecast = forecasts.find(
-    (forecast) => forecast.date === selectedDate
-  );
+	const selectedForecast = forecasts.find(
+		(forecast) => forecast.date === selectedDate
+	);
 
-  const handleForecastSelect = (date) => {
-    setSelectedDate(date);
-  };
+	const handleForecastSelect = (date) => {
+		setSelectedDate(date);
+	};
 
-  const handleCitySelect = () => {
-    getForecasts(
-      setErrorMessage,
-      setLocation,
-      setForecasts,
-      setSelectedDate,
-      searchText
-    );
-  };
-  return (
-    <div className="weather-app">
-      <div>
-        <LocationDetails
-          city={location.city}
-          country={location.country}
-          isError={errorMessage}
-        />
-        <SearchForm
-          onSubmit={handleCitySelect}
-          searchText={searchText}
-          setSearchText={setSearchText}
-        />
-        <ForecastSummaries
-          forecasts={forecasts}
-          isError={errorMessage}
-          onForecastSelect={handleForecastSelect}
-        />
-        {selectedForecast && (
-          <ForecastDetails forecast={selectedForecast} isError={errorMessage} />
-        )}
-      </div>
-    </div>
-  );
+	const handleCitySelect = () => {
+		getForecasts(
+			setErrorMessage,
+			setLocation,
+			setForecasts,
+			setSelectedDate,
+			searchText
+		);
+	};
+	return (
+		<div className="weather-app">
+			<div className="modal">
+				<div>
+					<LocationDetails
+						city={location.city}
+						country={location.country}
+						isError={errorMessage}
+					/>
+					<SearchForm
+						onSubmit={handleCitySelect}
+						searchText={searchText}
+						setSearchText={setSearchText}
+					/>
+				</div>
+				<section>
+					<div className="container">
+						<ForecastSummaries
+							forecasts={forecasts}
+							isError={errorMessage}
+							onForecastSelect={handleForecastSelect}
+						/>
+					</div>
+					{selectedForecast && (
+						<div className="container">
+							<ForecastDetails
+								forecast={selectedForecast}
+								isError={errorMessage}
+							/>
+						</div>
+					)}
+				</section>
+			</div>
+		</div>
+	);
 };
 
 export default App;
